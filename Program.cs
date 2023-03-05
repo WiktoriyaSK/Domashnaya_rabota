145e48c7802fd2be27fa0ff52831a8e6c019a7a5
vika-sk <viktoriya-skryabina@mail.ru>
2023-03-05 13:21:09 +0300

    Dom_rab

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// tasc 16
+Console.Clear();
+int n =  Convert.ToInt32(Console.ReadLine());
+int m =  Convert.ToInt32(Console.ReadLine());
+if (n *n == m || n == m * m)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
